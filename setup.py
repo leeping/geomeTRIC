535dd32bd6c2783af478e46e1235d221f3681d34
Daniel Smith <malorian@me.com>
2018-03-10 15:34:19 -0500

    Updates setup.py with new requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-setup(name='geometric',
-      description='Geometry optimization for quantum chemistry',
-      url='https://github.com/leeping/geomeTRIC',
-      author='Lee-Ping Wang, Chenchen Song',
-      packages=['geometric'],
-      entry_points={
-          'console_scripts': [
-              'geometric-optimize = geometric.optimize:main',
-          ]
-      }
-      )
+setup(
+    name='geometric',
+    description='Geometry optimization for quantum chemistry',
+    url='https://github.com/leeping/geomeTRIC',
+    author='Lee-Ping Wang, Chenchen Song',
+    packages=['geometric'],
+    entry_points={'console_scripts': [
+        'geometric-optimize = geometric.optimize:main',
+    ]},
+    install_requires=[
+        'numpy>=1.11',
+        'networkx',
+        'six',
+    ])
