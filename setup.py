807bd1ba71af88d7febd94e75f591a11bec0d0db
leeping <leeping@ucdavis.edu>
2018-07-26 21:21:01 +0800

    Add long_description for PyPI

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import versioneer
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='geometric',
     description='Geometry optimization for quantum chemistry',
     url='https://github.com/leeping/geomeTRIC',
     author='Lee-Ping Wang, Chenchen Song',
     packages=['geometric'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     entry_points={'console_scripts': [
         'geometric-optimize = geometric.optimize:main',
     ]},
