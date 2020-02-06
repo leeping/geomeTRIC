b6ed09096ce5afe4abc7aa494c728ffac1e09036
Lee-Ping Wang <leeping@ucdavis.edu>
2020-02-05 16:17:35 -0800

    Add path splitting function

--- geometric/nifty.py
+++ geometric/nifty.py
@@ -1008,6 +1008,21 @@ def click():
     return ans
 click.t0 = time.time()
 
+def splitall(path):
+    allparts = []
+    while 1:
+        parts = os.path.split(path)
+        if parts[0] == path:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == path: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            path = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
 # Back up a file.
 def bak(path, dest=None):
     oldf = path
