dc2e28deedf38bc3696876dd95bbd1280aed569c
T. K. Stenczel <tks32@cam.ac.uk>
2022-10-09 10:17:54 +0200

    integration test for ASE+XTB: transition state of HCN molecule

--- geometric/tests/addons.py
+++ geometric/tests/addons.py
@@ -73,6 +73,10 @@ using_ase = pytest.mark.skipif(
     _plugin_import("ase") is False,
     reason="could not find ase. please install the package to enable tests",
 )
+using_xtb = pytest.mark.skipif(
+    _plugin_import("xtb") is False,
+    reason="could not find ase. please install the package to enable tests",
+)
 
 
 # Points to the folder where the data files are installed.
