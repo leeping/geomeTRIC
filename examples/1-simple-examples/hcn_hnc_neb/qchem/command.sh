9e7710f70b526035898a8dd2bf94836cffa97f5c
Heejune Park <heepark@ucdavis.edu>
2023-04-17 16:22:05 -0700

    NEB examples were added

---
+++ examples/1-simple-examples/hcn_hnc_neb/qchem/command.sh
@@ -0,0 +1,1 @@
+geometric-neb qchem.in --coords HCN.xyz --engine qchem --nt 10 --images 11 --nebk 0.2 > neb.out
---
+++ examples/1-simple-examples/hcn_hnc_neb/terachem/command.sh
@@ -0,0 +1,1 @@
+geometric-neb tera.in --coords HCN.xyz --engine tera --images 11 --nebk 0.2 > neb.out
