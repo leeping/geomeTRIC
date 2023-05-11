b443c1468cca5a4afa74cdeb9d7356aa0c19056d
Heejune Park <heepark@ucdavis.edu>
2023-05-10 17:51:36 -0700

    test_params.py fixed

--- geometric/tests/test_params.py
+++ geometric/tests/test_params.py
@@ -24,11 +24,10 @@ def test_parse_command_line_opt_args(localizer):
 def test_parse_command_line_neb_args(localizer):
     shutil.copy2(os.path.join(datad, 'hcn_neb_input.psi4in'), os.getcwd())
     shutil.copy2(os.path.join(datad, 'hcn_neb_input.xyz'), os.getcwd())
-    args = geometric.params.parse_neb_args('--engine psi4 --nebew 5 --nebk 10.3 --plain 3 --images 7 --optep yes hcn_neb_input.psi4in hcn_neb_input.xyz'.split())
+    args = geometric.params.parse_neb_args('--engine psi4 --nebk 10.3 --plain 3 --images 7 --optep yes hcn_neb_input.psi4in hcn_neb_input.xyz'.split())
     assert args['engine'] == 'psi4'
     assert args['input'] == 'hcn_neb_input.psi4in'
     assert args['chain_coords'] == 'hcn_neb_input.xyz'
     assert args['optep'] == True
     assert args['images'] == 7
     assert args['nebk'] == 10.3
-    assert args['nebew'] == 5
