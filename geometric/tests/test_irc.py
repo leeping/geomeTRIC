970e37e49038fb61bb46f86c39e89bdb238554d1
Heejune Park <heepark@ucdavis.edu>
2023-09-21 13:43:30 -0700

    irc test updated (smaller stepsize)

--- geometric/tests/test_irc.py
+++ geometric/tests/test_irc.py
@@ -18,7 +18,7 @@ def test_hcn_irc_psi4(localizer):
     """
     shutil.copy2(os.path.join(datad, 'hcn_irc.psi4in'), os.path.join(os.getcwd(), 'hcn_irc.psi4in'))
     progress = geometric.optimize.run_optimizer(engine='psi4', input='hcn_irc.psi4in', converge=['set', 'GAU_LOOSE'],
-                                                nt=4, reset=False, trust=0.1, irc=True, maxiter=50)
+                                                nt=4, reset=False, trust=0.05, irc=True, maxiter=50)
     e_ref1 = -92.35408411
     e_ref2 = -92.33971205
     max_e = np.max(progress.qm_energies)
@@ -33,5 +33,5 @@ def test_hcn_irc_psi4(localizer):
     assert np.isclose(min(reac_e, prod_e), e_ref1)
     assert np.isclose(max(reac_e, prod_e), e_ref2)
 
-    # Check that the IRC converged in less than 50 iterations
-    assert len(progress) < 80
+    # Check that the IRC converged in less than 100 iterations
+    assert len(progress) < 100
